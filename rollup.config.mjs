9b565fce8b77a3e6b8dcf0da9b2b60298caf43e5
alanjoelschneider <alanjoelschneider@hotmail.com>
2023-05-25 11:56:29 -0300

    Fix entry file in rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import terser from '@rollup/plugin-terser';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: './src/index.ts',
+  input: './src/main.ts',
   output: {
     format: 'iife',
     file: './dist/bundle.js',
